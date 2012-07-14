80a6b97ddee3982edde0312fd73b6518d5f36b4d
Sean Dohring <sean@sunlis.ca>
2012-07-14 15:52:02 -0400

    Fix Issue #2
    Added new direct_config function that accepts an object containing
    arguments for direct. This closes Issue #2.
    
    Also added a 'call before' argument to direct, which gets called with
    an object containing the method, args and callback. I'm not sure how
    useful this will prove to be, but it was easy to add and might come in
    handy.

--- js/direct.js
+++ js/direct.js
@@ -1,12 +1,33 @@
-var direct = function(method,args,callback){
+var direct = function(method, args, callback, callbefore) {
 	var argString = "";
 	for(arg in args){
 		argString += "&" + arg + "=" + args[arg];
 	}
-	$.ajax({
-		url: "direct.php?method=" + method + argString,
-		complete: function(data){
-			callback($.parseJSON(data.responseText));
-		}
-	});
+	if (is_defined(callbefore)) {
+		callbefore({
+			'method':method,
+			'args':args,
+			'callback':callback
+		});
+	}
+	if(is_defined(method)){
+		$.ajax({
+			url: "direct.php?method=" + method + argString,
+			complete: function(data){
+				if (is_defined(callback)) {
+					callback($.parseJSON(data.responseText));
+				}
+			}
+		});
+	}else{
+		console.log("No method provided! Aborted direct call");
+	}
 };
+// simple wrapper to allow argument passing via an object
+var direct_config = function(config) {
+	direct(config['method'], config['args'], config['callback'], config['callbefore']);
+}
+
+var is_defined = function(what){
+	return (what !== undefined && what !== null);
+}
