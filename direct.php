b36129d9fbadc550d066df501f558a36fd8d3a0e
Sean Dohring <sean@sunlis.ca>
2012-07-14 15:34:27 -0400

    Fix issue #1
    Added error catch to direct.php to be more polite when a class cannot
    be found

--- direct.php
+++ direct.php
@@ -1,10 +1,12 @@
 <?php
 
+// make sure a method name is provided
 if (!isset($_GET['method'])) {
  	echo json_encode(array("error" => "'method' GET variable must be specified."));
  	exit;
 }
 
+// magic function that looks for an un-loaded class and tries to load it
 function __autoload( $className ) {	
 	$names = array(
 		strtolower($className),
@@ -29,7 +31,7 @@ function __autoload( $className ) {
 	while($file = readdir($handle)){
 		if($file == "." || $file == "..") continue;
 		foreach($suffixes as $suffix){
-			if($lowerClassName . $suffix == strtolower($file)){
+			if(strtolower($className) . $suffix == strtolower($file)){
 				include_once(__DIR__ . "/classes/" . $file);
 				break 2; // break out so the file handle gets closed
 			}
@@ -38,9 +40,16 @@ function __autoload( $className ) {
 	closedir($handle);
 }
 
+// see if the class can be found
+if (class_exists($_GET['method'], true) === false) {
+	echo json_encode(array("error" => "The direct class '".$_GET['method']."' does not exist or could not be found"));
+	exit;
+}
 
+// create a new instance of the direct class and call its 'run' method
 $method = new $_GET['method']($_GET['method']);
 $return = $method->run();
+// if the method returned anything, json encode it and pass it back to Javascript
 if(isset($return)){
 	echo json_encode($return);
 	exit;
